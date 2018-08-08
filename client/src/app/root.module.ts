08a5976db63097d5ab60f6e2e08f937403ed4009
Mark Prykhoda <m.prykhoda@gmail.com>
2018-08-08 11:46:06 +0300

    simple component with d3.js chart

--- client/src/app/root.module.ts
+++ client/src/app/root.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { RootComponent } from '@app/root.component';
 import { FeatureModule } from './features/feature.module';
 import { ServiceModule } from './services/service.module';
+import { TestChartComponent } from './test-chart/test-chart.component';
 
 @NgModule({
-	declarations: [RootComponent],
+	declarations: [RootComponent, TestChartComponent],
 	imports: [BrowserModule, FeatureModule, ServiceModule],
 	providers: [],
 	bootstrap: [RootComponent]
---
+++ client/src/app/test-chart/test-data.ts
@@ -0,0 +1,11 @@
+export interface Stage {
+	stage: String;
+	count: Number;
+}
+
+export const data: Stage[] = [
+	{ stage: '0', count: 3300 },
+	{ stage: '1', count: 450 },
+	{ stage: '2', count: 190 },
+	{ stage: '3', count: 114 }
+];
