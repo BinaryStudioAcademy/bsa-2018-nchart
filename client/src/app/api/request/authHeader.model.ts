2a7a2022f7e8f358f04b04afaf575474976da0de
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-09 20:03:51 +0300

    delete header models

--- client/src/app/api/request/authHeader.model.ts
+++
@@ -1,7 +0,0 @@
-import { Header } from '@app/api/request/header.model';
-
-export class AuthHeader extends Header {
-	constructor(value: string) {
-		super('Authorization', value);
-	}
-}
--- client/src/app/api/request/header.model.ts
+++
@@ -1,7 +0,0 @@
-export class Header {
-	constructor(public readonly name: string, public value: string) {}
-
-	toString(): string {
-		return `${this.name}: ${this.value}`;
-	}
-}
