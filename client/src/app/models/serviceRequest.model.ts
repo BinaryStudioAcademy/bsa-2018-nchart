1861876fe97768875c427c27edeb4c0f4863bda1
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-09 20:57:16 +0300

    fix serviceRequest

--- client/src/app/models/serviceRequest.model.ts
+++ client/src/app/models/serviceRequest.model.ts
@@ -2,20 +2,10 @@ import { HttpHeaders } from '@angular/common/http';
 import { RequestType } from '@app/models/requestType.model';
 
 export class ServiceRequest {
-	public readonly type: RequestType;
-	public readonly payload: any;
-	public readonly url: string;
-	public readonly headers: HttpHeaders;
-
 	constructor(
-		type: RequestType,
-		url: string,
-		headers?: HttpHeaders,
-		payload?: any
-	) {
-		this.type = type;
-		this.url = url;
-		this.headers = headers ? headers : new HttpHeaders();
-		this.payload = payload ? payload : null;
-	}
+		public readonly type: RequestType,
+		public readonly url: string,
+		public readonly headers?: HttpHeaders,
+		public readonly payload?: any
+	) {}
 }
