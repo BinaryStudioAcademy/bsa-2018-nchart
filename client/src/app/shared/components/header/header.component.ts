7940437b06773cd2b61e171b7c3144611f6adca6
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2018-08-11 14:27:18 +0300

    basic layout of header

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,19 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+	@Input()
+	projectName = 'Project name';
+	@Input()
+	isAuthorized = false;
+
 	items: MenuItem[];
+	authItems: MenuItem[];
+	userButtons;
 
 	ngOnInit() {
-		this.items = [
-			{ label: 'Stats' },
-			{ label: 'Calendar' },
-			{ label: 'Documentation' },
-			{ label: 'Support' },
-			{ label: 'Social' }
+		this.authItems = [
+			{ label: 'Projects', routerLink: ['/app/projects'] },
+			{ label: 'Companies', routerLink: ['/app/companies'] }
 		];
+		this.items = [
+			{
+				label: this.projectName,
+				routerLink: ['/app/projects/id'],
+				style: { 'margin-right': '100px', color: 'red' }
+			}
+		];
+		this.userButtons = [
+			{
+				label: 'Sign out',
+				routerLink: ['/'],
+				command: () => {
+					this.onClick();
+				}
+			}
+		];
+	}
+
+	onClick() {
+		this.isAuthorized = !this.isAuthorized;
+		this.isAuthorized
+			? this.items.push(...this.authItems)
+			: this.items.splice(1);
+	}
+
+	getButtonClasses() {
+		return {
+			'header-button': true
+		};
+	}
+	getUserButtonClasses() {
+		return {
+			'header-button': true,
+			'header-user-button': true
+		};
 	}
 }
