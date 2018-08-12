c7de996f18e4e536a0284f3af3ff43658ae1f24b
Mark Prykhoda <m.prykhoda@gmail.com>
2018-08-12 09:48:53 +0300

    First commit basic sekeleton

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,6 +8,17 @@ import { ProjectsComponent } from '@app/core/projects/projects.component';
 import { RouterModule } from '@app/router/router.module';
 import { APIModule } from '@app/api/api.module';
 import { SharedModule } from '@app/shared/shared.module';
+import { StepperComponent } from '@app/core/project/stepper/stepper.component';
+import { LoadDataComponent } from './project/load-data/load-data.component';
+import { DataTableComponent } from './project/data-table/data-table.component';
+import { ListChartsComponent } from './project/list-charts/list-charts.component';
+import { CustomSettingsComponent } from './project/custom-settings/custom-settings.component';
+import { CustomChartComponent } from './project/custom-chart/custom-chart.component';
+import { CustomizeChartComponent } from './project/custom-chart/customize-chart/customize-chart.component';
+import { ChartComponent } from './project/custom-chart/chart/chart.component';
+import { ExportComponent } from './project/export/export.component';
+import { ChartPreviewComponent } from './project/list-charts/chart-preview/chart-preview.component';
+import { HeaderComponent } from './project/header/header.component';
 
 @NgModule({
 	declarations: [
@@ -16,7 +27,18 @@ import { SharedModule } from '@app/shared/shared.module';
 		CompanyComponent,
 		LoginComponent,
 		ProjectComponent,
-		ProjectsComponent
+		ProjectsComponent,
+		StepperComponent,
+		LoadDataComponent,
+		DataTableComponent,
+		ListChartsComponent,
+		CustomSettingsComponent,
+		CustomChartComponent,
+		CustomizeChartComponent,
+		ChartComponent,
+		ExportComponent,
+		ChartPreviewComponent,
+		HeaderComponent
 	],
 	imports: [RouterModule, APIModule, SharedModule],
 	exports: []
