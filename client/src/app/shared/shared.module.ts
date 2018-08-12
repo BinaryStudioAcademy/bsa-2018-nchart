964c42ed9206fa3e55155d898f6e5105b232ea76
korspace <jkors.00.jk@gmail.com>
2018-08-12 18:05:08 +0300

    Tab view fixed

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { UIKitModule } from '@app/shared/uikit.module';
 import { ActionButtonComponent } from './components/button/action-button/action-button.component';
-import { TabsGroupComponent } from './components/tabs/tabs-group/tabs-group.component';
 import { FormFieldModule } from './components/form-field/form-field.module';
 
 @NgModule({
 	imports: [UIKitModule, FormFieldModule],
-	exports: [ActionButtonComponent, TabsGroupComponent, FormFieldModule],
-	declarations: [ActionButtonComponent, TabsGroupComponent]
+	exports: [ActionButtonComponent, FormFieldModule],
+	declarations: [ActionButtonComponent]
 })
 export class SharedModule {}
--- client/src/stories/tabs.stories.ts
+++ client/src/stories/tabs.stories.ts
@@ -1,76 +1,79 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
 import '@storybook/addon-notes/register';
 import { TabViewModule } from 'primeng/tabview';
-import { TabsGroupComponent } from '../app/shared/components/tabs/tabs-group/tabs-group.component';
+
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ButtonComponent } from '../app/shared/components/button/button/button.component';
+
 
 storiesOf('Tabs', module)
 	.addDecorator(
 		moduleMetadata({
 			imports: [
-				TabViewModule
+				TabViewModule, ButtonModule
 			],
-			declarations: []
+			declarations: [ButtonComponent]
 		})
 	)
 
 	.add('Simple tabs', () => ({
-		component: TabsGroupComponent,
-		props: {
-			tabs: [{
-				header: 'Open',
-				content: '',
-				disabled: false
-			}, {
-				header: 'Done'
-			}, {
-				header: 'Disabled',
-				disabled: true
-			}],
-			selectedTab: 1,
-			change: function() { }
-		}
+		template: `<p-tabView 	[activeIndex]="0"
+								(onChange)="{}">
+						<p-tabPanel header="One">
+							<h2>Content 1</h2>
+							<app-button label="lalal"></app-button>
+						</p-tabPanel>
+						<p-tabPanel header="Two">
+							<h2>Content 2</h2>
+							<app-button label="lol"></app-button>
+						</p-tabPanel>
+						<p-tabPanel header="Fife" [disabled]="true">
+						</p-tabPanel>
+					</p-tabView>`,
+		props: {}
 	}))
-
 	.add('Tabs with icons', () => ({
-			component: TabsGroupComponent,
-			props: {
-				tabs: [{
-					header: 'Open',
-					content: '',
-					disabled: false,
-					rightIcon: 'fas fa-check'
-				}, {
-					header: 'Done',
-					closable: false,
-					rightIcon: 'fas fa-user'
-				}, {
-					header: 'Disabled',
-					closable: false,
-					disabled: true,
-					rightIcon: 'fas fa-user'
-				}],
-				selectedTab: 0,
-				change: function() { }
-			}
-		}))
-
-		.add('Closable tab', () => ({
-			component: TabsGroupComponent,
-			props: {
-				tabs: [{
-					header: 'Open',
-					content: '',
-					disabled: false,
-					closable: true
-				}, {
-					header: 'Done',
-					closable: true
-				}, {
-					header: 'Disabled',
-					disabled: true,
-					closable: true
-				}],
-				selectedTab: 0,
-				change: function() { }
-			}
-		}));
+		template: `<p-tabView 	[activeIndex]="0"
+								(onChange)="{}">
+						<p-tabPanel
+								header="One"
+								leftIcon="fa fa-user"
+						>
+							Content 1
+						</p-tabPanel>
+						<p-tabPanel
+								header="One"
+								leftIcon="fa fa-user"
+								rightIcon="fa fa-search"
+						>
+							Content 2
+						</p-tabPanel>
+						<p-tabPanel
+								header="One"
+								leftIcon="fa fa-check"
+						>
+							Content 33
+						</p-tabPanel>
+					</p-tabView>`,
+		props: {}
+	}))
+	.add('Closable Tabs', () => ({
+		template: `<p-tabView 	[activeIndex]="0"
+								(onChange)="{}">
+						<p-tabPanel
+								header="One"
+								[closable]="true"
+						>
+							<h2>Content 2</h2>
+						</p-tabPanel>
+						<p-tabPanel
+								header="Three"
+								[closable]="true"
+								[disabled]="true"
+						>
+							<h2>Content 1</h2>
+						</p-tabPanel>
+					</p-tabView>`,
+		props: {}
+	}));
