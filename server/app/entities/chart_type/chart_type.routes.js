aa8d9dfacb06cffe6b827ac157ad9bd9f5fbcef4
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2018-08-13 10:19:03 +0300

    NC-38 Implement route for fetching chart types

---
+++ server/app/entities/chart_type/chart_type.routes.js
@@ -0,0 +1,19 @@
+const chartType = require('express').Router();
+const chartTypeService = require('./chart_type.service');
+const PayloadGeneratorService = require('../../common/services/payload-generator.service');
+
+chartType.get('/', (req, res, next) => {
+	chartTypeService
+		.getAll()
+		.then(PayloadGeneratorService.nextWithData(next, res))
+		.catch(next);
+});
+
+chartType.get('/:id', (req, res, next) => {
+	chartTypeService
+		.getById(Number(req.params.id))
+		.then(PayloadGeneratorService.nextWithData(next, res))
+		.catch(next);
+});
+
+module.exports = chartType;
