f846f4c79698786d46744b5cffe2a31887892a9c
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-14 08:37:36 +0300

    add form validators

--- client/src/app/shared/components/form-field/form-validators.ts
+++ client/src/app/shared/components/form-field/form-validators.ts
@@ -1,7 +1,19 @@
-import { AbstractControl, ValidatorFn, Validators } from '@angular/forms';
+import {
+	AbstractControl,
+	ValidatorFn,
+	Validators,
+	ValidationErrors
+} from '@angular/forms';
 
 interface ValidationMessage {
 	[key: string]: any;
+}
+
+function validatePattern(
+	pattern: string | RegExp,
+	control: AbstractControl
+): ValidationErrors {
+	return Validators.pattern(pattern).call(null, control);
 }
 
 export function requiredValidator(msg?: string): ValidatorFn {
@@ -42,6 +54,82 @@ export function maxLengthValidator(
 		if (control.value.length > maxLength) {
 			return {
 				minLength: `${msg} ${maxLength}`
+			};
+		} else {
+			return null;
+		}
+	};
+}
+
+export function emailValidator(
+	msg?: string,
+	emailPattern?: string | RegExp
+): ValidatorFn {
+	return (control: AbstractControl): ValidationMessage => {
+		const validationErrors = emailPattern
+			? validatePattern(emailPattern, control)
+			: Validators.email(control);
+
+		if (validationErrors) {
+			return {
+				required: msg || 'Invalid email'
+			};
+		} else {
+			return null;
+		}
+	};
+}
+
+export function pattertValidator(
+	msg?: string,
+	patternExpression?: string | RegExp
+): ValidatorFn {
+	return (control: AbstractControl): ValidationMessage => {
+		const validationErrors = validatePattern(patternExpression, control);
+		if (validationErrors) {
+			return {
+				required: msg || 'Not match with pattern expression'
+			};
+		} else {
+			return null;
+		}
+	};
+}
+
+export function passwordValidator(
+	msg?: string,
+	customPattern?: string | RegExp
+): ValidatorFn {
+	return (control: AbstractControl): ValidationMessage => {
+		const defaultPattern = /^(?=.*[a-zа-яэіїє])(?=.*[A-ZА-ЯЭІЇЄ])(?=.*\d)(?=.*[#$^+=!*()@%&]).+$/;
+
+		const validationErrors = customPattern
+			? validatePattern(customPattern, control)
+			: validatePattern(defaultPattern, control);
+
+		if (validationErrors) {
+			return {
+				required:
+					msg ||
+					'Password should contain alphabetic, numeric and special characters'
+			};
+		} else {
+			return null;
+		}
+	};
+}
+
+export function passwordMatchValidator(
+	password: AbstractControl,
+	msg?: string
+): ValidatorFn {
+	return (control: AbstractControl): ValidationMessage => {
+		const passwordValue = password.value;
+		const confirmPasswordValue = control.value;
+
+		if (passwordValue !== confirmPasswordValue) {
+			return {
+				required: msg || 'Passwords aren`t match'
 			};
 		} else {
 			return null;
