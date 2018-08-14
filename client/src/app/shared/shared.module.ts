a309c24670aab97b9011b162a0a635c589086f6a
Vasyl Terletskyi <vasyl.terletskyi@binary-studio.com>
2018-08-14 21:50:41 +0300

    Add animation to main block

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,22 @@ import { UIKitModule } from '@app/shared/uikit.module';
 import { FormFieldModule } from '@app/shared/components/form-field/form-field.module';
 import { ActionButtonComponent } from '@app/shared/components/button/action-button/action-button.component';
 import { HeaderModule } from '@app/shared/components/header/header.module';
+import { MainBlockComponent } from './components/main-block/main-block.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-	imports: [UIKitModule, FormFieldModule, HeaderModule],
-	exports: [ActionButtonComponent, FormFieldModule, HeaderModule],
-	declarations: [ActionButtonComponent]
+	imports: [
+		UIKitModule,
+		FormFieldModule,
+		HeaderModule,
+		BrowserAnimationsModule
+	],
+	exports: [
+		ActionButtonComponent,
+		FormFieldModule,
+		HeaderModule,
+		MainBlockComponent
+	],
+	declarations: [ActionButtonComponent, MainBlockComponent]
 })
 export class SharedModule {}
