40868fdc0ae27789556be32058bf73f10b1b0aca
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2018-08-14 14:30:59 +0300

    attempt to use travis

--- server/app/index.js
+++ server/app/index.js
@@ -4,12 +4,14 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const initializeAPIRoutes = require('./routes');
 const dbConnect = require('./db/dbconnect');
+const path = require('path');
+
 const {
 	successOrEmptyPayload,
 	errorPayload
 } = require('./common/middleware/payload.middleware');
 
-dbConnect();
+//dbConnect();
 
 const app = express();
 
@@ -24,7 +26,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-initializeAPIRoutes(app);
+const staticPath = path.resolve(`${__dirname}/../../client/dist/client`);
+app.use(express.static(staticPath));
+
+//initializeAPIRoutes(app);
 
 // pre-sending middleware
 app.use(successOrEmptyPayload);
