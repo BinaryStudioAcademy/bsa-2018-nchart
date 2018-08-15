609b1cefff333ccabba6f1298232af5356b7a493
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-15 09:31:57 +0300

    add login service

--- client/src/app/core/login/login.component.ts
+++ client/src/app/core/login/login.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import {
-	requiredValidator,
-	emailValidator,
-	minLengthValidator,
-	passwordValidator,
-	passwordMatchValidator,
-	patternValidator,
-	maxLengthValidator
-} from '@app/shared/components/form-field/form-validators';
+import { FormGroup } from '@angular/forms';
+
+import { LoginService } from '@app/services/login.service';
 
 @Component({
 	selector: 'app-login',
@@ -19,39 +12,16 @@ export class LoginComponent implements OnInit {
 	loginForm: FormGroup;
 	registerForm: FormGroup;
 
-	constructor(private formBuilder: FormBuilder) {
-		this.loginForm = this.formBuilder.group({
-			email: new FormControl('', [requiredValidator(), emailValidator()]),
-			password: new FormControl('', [requiredValidator()])
-		});
-
-		const passwordControl = new FormControl('', [
-			requiredValidator(),
-			minLengthValidator('Minimum length of password is', 8),
-			passwordValidator()
-		]);
-
-		const confirmPasswordControl = new FormControl('', [
-			requiredValidator(),
-			passwordMatchValidator(passwordControl)
-		]);
-
-		this.registerForm = this.formBuilder.group({
-			name: new FormControl('', [
-				requiredValidator(),
-				patternValidator(
-					'Name should contain only alphabetic characters',
-					/^[a-zа-яэіїє]+$/i
-				),
-				maxLengthValidator('Maximum length of name is', 100)
-			]),
-			email: new FormControl('', [requiredValidator(), emailValidator()]),
-			password: passwordControl,
-			confirmPassword: confirmPasswordControl
-		});
+	constructor(private loginService: LoginService) {
+		this.createForms();
 	}
 
 	ngOnInit() {}
+
+	private createForms() {
+		this.loginForm = this.loginService.createLoginForm();
+		this.registerForm = this.loginService.createRegisterForm();
+	}
 
 	resetForms() {
 		this.loginForm.reset();
