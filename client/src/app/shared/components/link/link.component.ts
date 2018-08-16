2c98b4d6d4482f9ff203228dfc451f8cabd9c684
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-16 12:51:47 +0300

    add link component

--- client/src/app/shared/components/link/link.component.ts
+++ client/src/app/shared/components/link/link.component.ts
@@ -1,11 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-link',
 	templateUrl: './link.component.html'
 })
 export class LinkComponent implements OnInit {
+	@Input()
+	type: 'primary' | 'secondary' | 'underline' | 'shape' = 'primary';
+
+	@Input()
+	link: string = null;
+
+	@Input()
+	label = 'Text Link';
+
+	@Input()
+	icon: string = null;
+
 	constructor() {}
 
 	ngOnInit() {}
+
+	getClasses() {
+		return {
+			['ui-link']: true,
+			['ui-link-primary']: this.type === 'primary',
+			['ui-link-secondary']: this.type === 'secondary',
+			['ui-link-underline']: this.type === 'underline',
+			['ui-link-shape']: this.type === 'shape'
+		};
+	}
+
+	getIconClasses() {
+		const iconClasses = this.icon
+			.split(' ')
+			.reduce((accum, curr) => Object.assign(accum, { [curr]: true }), {
+				['icon']: true
+			});
+
+		return iconClasses;
+	}
 }
