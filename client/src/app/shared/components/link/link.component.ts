4dbd380096730cf31387028dbfd805a270f5271c
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-16 19:20:32 +0300

    refactor link component

--- client/src/app/shared/components/link/link.component.ts
+++ client/src/app/shared/components/link/link.component.ts
@@ -6,38 +6,66 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LinkComponent implements OnInit {
 	@Input()
-	type: 'primary' | 'secondary' | 'underline' | 'shape' = 'primary';
+	type: 'primary' | 'secondary' | 'underline' = 'primary';
 
 	@Input()
 	link: string = null;
 
 	@Input()
-	label = 'Text Link';
+	label: string = null;
 
 	@Input()
 	icon: string = null;
 
+	@Input()
+	shape: string = null;
+
+	@Input()
+	displayWith: 'icon' | 'shape';
+
 	constructor() {}
 
-	ngOnInit() {}
+	private static mapClassesToObject(
+		classes: string | string[]
+	): { [key: string]: boolean } {
+		const classesArray = Array.isArray(classes)
+			? classes
+			: classes.split(' ');
+
+		return classesArray.reduce((obj, item) => {
+			obj[item] = true;
+			return obj;
+		}, {});
+	}
+
+	ngOnInit() {
+		this.displayWith = this.detectDisplayWith();
+	}
+
+	detectDisplayWith() {
+		return this.displayWith
+			? this.displayWith
+			: this.icon
+				? 'icon'
+				: this.shape
+					? 'shape'
+					: null;
+	}
 
 	getClasses() {
 		return {
 			['ui-link']: true,
 			['ui-link-primary']: this.type === 'primary',
 			['ui-link-secondary']: this.type === 'secondary',
-			['ui-link-underline']: this.type === 'underline',
-			['ui-link-shape']: this.type === 'shape'
+			['ui-link-underline']: this.type === 'underline'
 		};
 	}
 
 	getIconClasses() {
-		const iconClasses = this.icon
-			.split(' ')
-			.reduce((accum, curr) => Object.assign(accum, { [curr]: true }), {
-				['icon']: true
-			});
+		return LinkComponent.mapClassesToObject(`icon ${this.icon}`);
+	}
 
-		return iconClasses;
+	getShapeClasses() {
+		return LinkComponent.mapClassesToObject(`shape ${this.shape}`);
 	}
 }
