73cd66e891acc521f455e65c57f182e9a2ee06f4
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2018-08-18 03:30:52 +0300

    create empty service for chart

--- client/src/app/services/service.module.ts
+++ client/src/app/services/service.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { StoreService } from '@app/services/store.service';
 import { FormService } from '@app/services/form.service';
 import { LoginService } from '@app/services/login.service';
+import { ChartService } from '@app/services/chart.service';
 
 @NgModule({
-	providers: [StoreService, FormService, LoginService]
+	providers: [StoreService, FormService, LoginService, ChartService]
 })
 export class ServiceModule {}
