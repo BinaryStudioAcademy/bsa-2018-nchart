956f4d5c6b13177aed4ca3f984b956b67086484d
korspace <jkors.00.jk@gmail.com>
2018-08-19 15:03:12 +0300

    Header project name changing implemented

--- client/src/app/core/app/app.component.ts
+++ client/src/app/core/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { StoreService } from '@app/services/store.service';
+import { project } from '@app/store/selectors/projects.selectors';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
 	selector: 'app-app',
 	templateUrl: 'app.component.html',
 	styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements OnInit {
-	constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+	projectName: string;
+	getName: () => void;
 
-	ngOnInit() {}
+	constructor(private storeService: StoreService, private cd: ChangeDetectorRef) {}
+
+	ngOnInit() {
+		this.getName = this.storeService.connect([
+			{
+				subscriber: prj => {
+					if (prj) {
+						this.projectName = prj.name;
+					}
+				},
+				selector: project()
+			}
+		]);
+		this.cd.detectChanges();
+	}
+
+	ngOnDestroy() {}
 }
--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
 	@Input()
-	projectName = 'Project name';
+	projectName: string;
 	@Input()
 	isAuthorized = false;
 
@@ -14,16 +15,22 @@ export class HeaderComponent implements OnInit {
 	authItems: MenuItem[];
 	profileItems: MenuItem[];
 
+	constructor() {}
+
+	ngOnChanges() {
+		if (this.projectName) {
+			this.items[0].label = this.projectName;
+		}
+	}
+
 	ngOnInit() {
+		this.items = [{
+				label: this.projectName || '',
+				routerLink: ['/app/projects/draft']
+		}];
 		this.authItems = [
 			{ label: 'Projects', routerLink: ['/app/projects'] },
 			{ label: 'Companies', routerLink: ['/app/companies'] }
-		];
-		this.items = [
-			{
-				label: this.projectName,
-				routerLink: ['/app/projects/draft']
-			}
 		];
 		this.profileItems = [
 			{
