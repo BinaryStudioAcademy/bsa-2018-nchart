539b40e8d112f24a855bb2bcd03231736221e4c0
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-19 19:23:32 +0300

    add router effects, redirect from login/register

---
+++ client/src/app/store/effects/router.effects.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Effect, Actions, ofType } from '@ngrx/effects';
+import { map, tap } from 'rxjs/operators';
+import { Go } from '../actions/router/router.actions';
+import { RouterActionConstants } from '@app/store/actions/router/router.action-types';
+
+@Injectable()
+export class RouterEffects {
+	@Effect({ dispatch: false })
+	navigate$ = this.actions$.pipe(
+		ofType(RouterActionConstants.GO),
+		map((action: Go) => action.payload),
+		tap(({ path, query: queryParams, extras }) =>
+			this.router.navigate(path, { queryParams, ...extras })
+		)
+	);
+
+	@Effect({ dispatch: false })
+	navigateBack$ = this.actions$.pipe(
+		ofType(RouterActionConstants.BACK),
+		tap(() => this.location.back())
+	);
+
+	@Effect({ dispatch: false })
+	navigateForward$ = this.actions$.pipe(
+		ofType(RouterActionConstants.FORWARD),
+		tap(() => this.location.forward())
+	);
+
+	constructor(
+		private actions$: Actions,
+		private router: Router,
+		private location: Location
+	) {}
+}
