54b8b441eced0eae3b7013b6a831f3c81e4b31b0
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2018-08-22 11:56:51 +0300

    set gcloud zone

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gcloud compute ssh $INSTANCE
+gcloud compute ssh $INSTANCE --zone=$ZONE
 docker kill $(docker ps -q)
 docker pull $DOCKER_IMAGE_NAME
 docker run -it -d -p 80:9000 --env-file '1.env' $DOCKER_IMAGE_NAME
