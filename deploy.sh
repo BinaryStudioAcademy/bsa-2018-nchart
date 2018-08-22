954b2284f87ac54a2a0a7f8abbac01c7b40e3897
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2018-08-22 13:52:20 +0300

    change dockerignore

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@ set -e
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker kill nchart && docker rm nchart && docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:9000 --name=nchart --env-file '1.env' $DOCKER_IMAGE_NAME"
-# sleep 30
-# npm run e2e_test
+gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker stop nchart && docker rm nchart && docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:9000 --name=nchart --env-file '1.env' $DOCKER_IMAGE_NAME"
