0cf4d14473db847b817dec7a9715a248a5945067
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2018-08-22 13:24:35 +0300

    add build number to gcloud ssh commands

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set -e
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker kill nchart && docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:9000 --name=nchart --env-file '1.env' $DOCKER_IMAGE_NAME"
+gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker kill nchart && docker pull $DOCKER_IMAGE_NAME:$TRAVIS_BUILD_NUMBER && docker run -it -d -p 80:9000 --name=nchart --env-file '1.env' $DOCKER_IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
 # sleep 30
 # npm run e2e_test
