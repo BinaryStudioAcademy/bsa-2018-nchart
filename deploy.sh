5d4fa104f3714d0a518ab766a19d9bb08c5f4927
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2018-08-23 09:50:53 +0300

    remove docker image before pull

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker stop nchart && docker rm nchart && docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:9000 --name=nchart --env-file '1.env' $DOCKER_IMAGE_NAME"
+gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker stop nchart && docker rm nchart && docker rmi -f $DOCKER_IMAGE_NAME && docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:9000 --name=nchart --env-file '1.env' $DOCKER_IMAGE_NAME"
