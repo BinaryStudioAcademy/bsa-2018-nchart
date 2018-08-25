63b88d89420568358625b4a6039400a0612a7e92
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-08-25 19:52:39 +0300

    add  app-auth guard, spiner to app component

--- client/src/app/router/app-routing.module.ts
+++ client/src/app/router/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '@app/core/login/login.component';
-import { AuthGuard } from '@app/services/guards/auth.guard';
+import { AuthGuard } from '@app/services/guards/app-auth.guard';
 import { ProjectComponent } from '@app/core/project/project.component';
 import { ProjectsComponent } from '@app/core/projects/projects.component';
 import { CompaniesComponent } from '@app/core/companies/companies.component';
@@ -18,11 +18,12 @@ const appRoutes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent,
-		canActivate: [AuthGuard]
+		canActivate: []
 	},
 	{
 		path: 'app',
 		component: AppComponent,
+		canActivate: [AuthGuard],
 		children: [
 			// {
 			// 	path: '',
--- client/src/app/services/guards/app-auth.guard.ts
+++ client/src/app/services/guards/app-auth.guard.ts
@@ -5,15 +5,28 @@ import {
 	RouterStateSnapshot
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { StoreService } from '@app/services/store.service';
+import { TokenService } from '@app/services/token.service';
+import { VerifyToken } from '@app/store/actions/user/user.actions';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+	constructor(
+		private storeService: StoreService,
+		private tokenService: TokenService
+	) {}
+
 	canActivate(
 		next: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean> | Promise<boolean> | boolean {
+		const token = this.tokenService.getToken();
+		if (token) {
+			this.storeService.dispatch(new VerifyToken({ token }));
+		}
+
 		return true;
 	}
 }
