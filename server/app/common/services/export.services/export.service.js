af27d65800b8d83aaf62e95c0321c19cc8fd9787
Mark Prykhoda <m.prykhoda@gmail.com>
2018-08-27 11:32:44 +0300

    Add createPng service

--- server/app/common/services/export.services/export.service.js
+++ server/app/common/services/export.services/export.service.js
@@ -1,20 +1,22 @@
 const PdfService = require('../export.services/pdf.service');
+const PngService = require('../export.services/png.service');
 
 class ExportService {
 	constructor() {
 		this.PdfService = PdfService;
+		this.PngService = PngService;
 	}
 
 	getFile(id, type) {
 		switch (type) {
-		case 'pdf':
-			return this.PdfService.createPdf(id);
-		case 'png':
-			return this.PngService.createPng(id);
-		case 'svg':
-			return this.SvgService.createSvg(id);
-		default:
-			return '';
+			case 'pdf':
+				return this.PdfService.createPdf(id);
+			case 'png':
+				return this.PngService.createPng(id);
+			case 'svg':
+				return this.SvgService.createSvg(id);
+			default:
+				return '';
 		}
 	}
 }
