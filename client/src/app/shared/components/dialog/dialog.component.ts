7d4635c08ebd34344511ddbf2c2c2893ed79a24f
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-09-02 17:21:19 +0200

    Header changes

--- client/src/app/shared/components/dialog/dialog.component.ts
+++ client/src/app/shared/components/dialog/dialog.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 import {
 	requiredValidator,
 	emailValidator
 } from '@app/shared/components/form-field/form-validators';
 import * as projectActions from '@app/store/actions/projects/projects.actions';
-import { StoreService } from '@app/services/store.service';
+import {StoreService} from '@app/services/store.service';
 
 @Component({
 	selector: 'app-dialog',
 	templateUrl: './dialog.component.html'
 })
 export class DialogComponent implements OnInit {
-	constructor(private storeService: StoreService) {}
+	constructor(private storeService: StoreService) {
+	}
 
 	@Input()
 	display: boolean;
@@ -45,6 +46,8 @@ export class DialogComponent implements OnInit {
 
 	share(value) {
 		this.storeService.dispatch(new projectActions.ShareProject(value));
+		this.formGroup.reset();
+		this.formGroup.setValue({projectId: this.projectId, email: null, accessLevelId: '3'});
 		this.displayChange.emit(false);
 	}
 }
